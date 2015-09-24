baeca1cc98deb636dbd5ea88edfb3efd7cb72b51
Marcin Sitko <marcin.sitko@live.com>
2015-09-24 19:43:49 +0200

    Added sql file

---
+++ Log4NetActivateOptionsIssue/CreateLogTable.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE [dbo].[LogNet](
+	[DateUtc] [datetime] NULL,
+	[Thread] [varchar](50) NULL,
+	[Level] [varchar](50) NULL,
+	[Logger] [varchar](200) NULL,
+	[User] [varchar](50) NULL,
+	[Message] [varchar](4000) NULL,
+	[Exception] [varchar](4000) NULL
+)
